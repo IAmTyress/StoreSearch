f24fee430ff5df7e33b43a9cc096b05d64127305
IAmTyReSS <malyshtim2000@yandex.ru>
2021-08-11 14:02:56 +0300

    Rename ViewController to SearchViewController

---
+++ StoreSearch/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Russ Rosaura on 8/11/21.
+//  Copyright © 2021 Malysh Tim. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
