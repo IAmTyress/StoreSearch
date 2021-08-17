d7c39d7168517b9cd3921ca6e29d376591259d54
IAmTyReSS <malyshtim2000@yandex.ru>
2021-08-17 14:50:02 +0300

    Customize pop-up view

---
+++ StoreSearch/DimmingPresentationController.swift
@@ -0,0 +1,15 @@
+//
+//  DimmingPresentationController.swift
+//  StoreSearch
+//
+//  Created by Russ Rosaura on 8/17/21.
+//  Copyright © 2021 Malysh Tim. All rights reserved.
+//
+
+import UIKit
+
+class DimmingPresentationController: UIPresentationController {
+    override var shouldRemovePresentersView: Bool {
+        return false
+    }
+}
