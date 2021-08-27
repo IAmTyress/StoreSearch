cb462c6e130abeeeb24ba4653740d75c506d1d50
IAmTyReSS <malyshtim2000@yandex.ru>
2021-08-27 15:38:04 +0300

    Add Russian localization

--- StoreSearch/Search.swift
+++ StoreSearch/Search.swift
@@ -76,11 +76,17 @@ class Search {
     
     private func iTunesURL(searchText: String, category: Category) -> URL {
         let kind = category.type
+        let locale = Locale.autoupdatingCurrent
+        let language = locale.identifier
+        let countryCode = locale.regionCode ?? "US"
         
         let encodedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
-        let urlString = "https://itunes.apple.com/search?" + "term=\(encodedText)&limit=200&entity=\(kind)"
+        let urlString = "https://itunes.apple.com/search?"
+            + "term=\(encodedText)&limit=200&entity=\(kind)"
+            + "&lang=\(language)&country=\(countryCode)"
         
         let url = URL(string: urlString)
+        print("URL: \(url!)")
         return url!
     }
     
